e8300accfd666d1346b8a0b51e938654cdfdb066
rfehn239 <rfehn239@gmail.com>
2023-07-08 01:46:27 +0900

    [add]メインプロジェクトを追加しました。

---
+++ src/MediaApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/MediaApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MediaApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
